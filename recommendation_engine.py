ee40c3afa0c67de712b028ae4c53e7eeeabc7580
shrewsbury928 <you@example.com>
2026-03-16 03:04:49 +0000

    wrong duplicate

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -1,6 +1,6 @@
 import random
 from playlist import Playlist
-from song_library_manager import SongLibraryManager as song_lib
+from songs_manager import SongLibrary as song_lib
 
 
 class RecommendationEngine:
